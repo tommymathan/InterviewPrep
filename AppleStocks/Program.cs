fbc11274ffa3776a30fa43dd7bb9c6dd0b4200e4
v-thomam <v-thomam@MININT-33IEDTB>
2018-01-31 18:05:57 -0800

    Highest profit from stocks problem

--- AppleStocks/Program.cs
+++ AppleStocks/Program.cs
@@ -10,6 +10,35 @@ namespace AppleStocks
     {
         static void Main(string[] args)
         {
+            int[] stockPrices = { 15, 4, 7, 9, };
+            int[] stockPrices2 = { 15, 4, 7, 9, 1, 19 };
+
+
+            int maxProfit = MaxProfitFromYesterday(stockPrices);
+            int maxProfit2 = MaxProfitFromYesterday(stockPrices2);
+            
+        }
+
+        public static int MaxProfitFromYesterday(int[] stockPricesFromYesterday)
+        {
+            if (stockPricesFromYesterday.Length < 2)
+            {
+                throw new ArgumentException("Getting a profit requires at least 2 prices",
+                    nameof(stockPricesFromYesterday));
+            }
+
+            int lowestPrice = stockPricesFromYesterday[0];
+            int maxProfit = stockPricesFromYesterday[1] - stockPricesFromYesterday[0];
+
+            for(int i = 1; i < stockPricesFromYesterday.Length; i++)
+            {
+                int currentPrice = stockPricesFromYesterday[i];
+                int currentProfit = currentPrice - lowestPrice;
+                lowestPrice = Math.Min(currentPrice, lowestPrice);
+                maxProfit = Math.Max(maxProfit, currentProfit);
+            }
+
+            return maxProfit;
         }
     }
 }
